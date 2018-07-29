7939eb42b08808ed1cc32a541d5e56593b4691c8
Jordi Puiggené <jpuiggene@naralabs.com>
2018-07-29 13:19:15 +0200

    Added Min age, max age and gender in Analysis Specs
    Squashed commit of the following:
    
    commit 010a6c606d08367bba281698c2cd688611f3b77d
    Author: Jordi Puiggené <jpuiggene@naralabs.com>
    Date:   Sun Jul 29 12:25:53 2018 +0200
    
        Added helper function to retrieve values from Schema fields
    
    commit 9ad85f6b1efcd832075a7096b55ed9f1835a8d06
    Author: Jordi Puiggené <jpuiggene@naralabs.com>
    Date:   Sun Jul 29 11:49:52 2018 +0200
    
        PEP8 styling
    
    commit d5bd6c18ab01c30612711ef6a7c54af702f32c32
    Author: Jordi Puiggené <jpuiggene@naralabs.com>
    Date:   Sun Jul 29 11:41:41 2018 +0200
    
        Added columns Age and Gender in Analysis Specifications views
    
    commit 4ea8cc5ca9d18c80f1661cc7dcf0aca62064ac97
    Author: Jordi Puiggené <jpuiggene@naralabs.com>
    Date:   Sun Jul 29 11:12:14 2018 +0200
    
        Added Min age, Max age and Gender in Analysis Specifications

--- bhp/lims/content/analysisspec.py
+++ bhp/lims/content/analysisspec.py
@@ -2,14 +2,77 @@
 #
 # Copyright 2018 Botswana Harvard Partnership (BHP)
 
+from Products.Archetypes.public import StringWidget
+from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
 from archetypes.schemaextender.interfaces import ISchemaModifier
 from bhp.lims import bhpMessageFactory as _
-from bhp.lims.validators import AnalysisSpecificationsValidator
 from bhp.lims.browser.widgets.analysisspecificationwidget import \
     AnalysisSpecificationWidget
+from bhp.lims.config import GENDERS_ANY
+from bhp.lims.validators import AnalysisSpecificationsValidator
+from bika.lims import bikaMessageFactory as _
+from bika.lims.browser.widgets import SelectionWidget
+from bika.lims.fields import ExtStringField
 from bika.lims.interfaces import IAnalysisSpec
 from zope.component import adapts
 from zope.interface import implements
+
+
+class AnalysisSpecsSchemaExtender(object):
+    adapts(IAnalysisSpec)
+    implements(IOrderableSchemaExtender)
+
+    def __init__(self, context):
+        self.context = context
+
+
+    fields = [
+        ExtStringField(
+            "Gender",
+            required=0,
+            vocabulary=GENDERS_ANY,
+            widget=SelectionWidget(
+                format="select",
+                label=_("Gender"),
+            )
+        ),
+        ExtStringField(
+            "Agefrom",
+            required=0,
+            widget=StringWidget(
+                label=_("Age from (inclusive)"),
+                description=_("Minimum age this specification will apply. "
+                              "Inclusive value (>=). Format example: 15y 5m 6d"),
+                maxlength=12,
+                size=12,
+            )
+        ),
+        ExtStringField(
+            "Ageto",
+            required=0,
+            widget=StringWidget(
+                label=_("Age to (inclusive)"),
+                description=_("Maximum age this specification will apply. "
+                              "Exclusive value (<). Format example: 85y 5m 6d"),
+                maxlength=12,
+                size=12,
+            )
+        ),
+    ]
+
+    def getOrder(self, schematas):
+        default = schematas['default']
+        default.remove('Gender')
+        default.remove('Agefrom')
+        default.remove('Ageto')
+        default.insert(default.index('ResultsRange'), 'Gender')
+        default.insert(default.index('ResultsRange'), 'Agefrom')
+        default.insert(default.index('ResultsRange'), 'Ageto')
+        schematas['default'] = default
+        return schematas
+
+    def getFields(self):
+        return self.fields
 
 
 class AnalysisSpecSchemaModifier(object):
