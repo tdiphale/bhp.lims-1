59d7ef7339dcfffebeddc9463204ae0c96b3ccc7
Ramon Bartl <rb@ridingbytes.com>
2018-09-06 08:17:56 +0200

    Integrate Courier Type into ID-Server (#94)

--- bhp/lims/content/courier.py
+++ bhp/lims/content/courier.py
@@ -3,10 +3,10 @@
 # Copyright 2018 Botswana Harvard Partnership (BHP)
 
 from AccessControl import ClassSecurityInfo
-from Products.Archetypes.public import *
 from bhp.lims.config import PRODUCT_NAME
 from bhp.lims.interfaces import ICourier
 from bika.lims.content.person import Person
+from Products.Archetypes.public import registerType
 from zope.interface import implements
 
 schema = Person.schema.copy()
@@ -15,10 +15,20 @@ schema = Person.schema.copy()
 schema['title'].required = 0
 schema['title'].widget.visible = False
 
+
 class Courier(Person):
+    """Defines a Courier for Shipment
+    """
     implements(ICourier)
     security = ClassSecurityInfo()
     displayContentsTab = False
     schema = schema
+
+    _at_rename_after_creation = True
+
+    def _renameAfterCreation(self, check_auto_id=False):
+        from bika.lims.idserver import renameAfterCreation
+        renameAfterCreation(self)
+
 
 registerType(Courier, PRODUCT_NAME)
