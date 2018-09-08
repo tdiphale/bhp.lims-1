d0a3daecf2d19ba96ece704226d7cf579300039a
Jordi Puiggené <jpuiggene@naralabs.com>
2018-09-08 07:23:40 +0200

    Promote after_transitions to ancestors

---
+++ bhp/lims/monkeys/workflow/analysisrequest/events.py
@@ -0,0 +1,15 @@
+from bika.lims.workflow import doActionFor
+
+def _promote_transition(obj, transition_id):
+    """Promotes the transition passed in to the object's parent and ancestor
+    :param obj: Analysis Request for which the transition has to be promoted
+    :param transition_id: Unique id of the transition
+    """
+    sample = obj.getSample()
+    if sample:
+        doActionFor(sample, transition_id)
+
+    # Promote the transition to its parent
+    parent = obj.getAncestors(all_ancestors=False)
+    if parent:
+        doActionFor(parent, transition_id)
